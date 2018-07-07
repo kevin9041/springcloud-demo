42a9bafa0c796d362f39440699ea371938b0a246
weihuanbo <weihuanbo@misrobot.com>
2018-07-07 18:28:15 +0800

    1 Signed-off-by: weihuanbo <weihuanbo@misrobot.com>

--- springcloudlearning-config-rabbitMQ-6001/src/main/java/com/misrobot/springcloud/Config_RabbitApplication.java
+++ springcloudlearning-config-rabbitMQ-6001/src/main/java/com/misrobot/springcloud/Config_RabbitApplication.java
@@ -4,10 +4,12 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @EnableRabbit
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Config_RabbitApplication {
 
