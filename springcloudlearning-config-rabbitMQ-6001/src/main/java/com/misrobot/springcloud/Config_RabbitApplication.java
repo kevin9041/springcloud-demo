1a320401bb80b483bd7adafec87ef9b74193564f
weihuanbo <weihuanbo@misrobot.com>
2018-07-27 16:49:13 +0800

    //@RefreshScope              ----------------->加载这里没有用 只能加在需要刷新的地方才可以 如${foo} //http://localhost:6001/bus/refresh Signed-off-by: weihuanbo <weihuanbo@misrobot.com>

--- springcloudlearning-config-rabbitMQ-6001/src/main/java/com/misrobot/springcloud/Config_RabbitApplication.java
+++ springcloudlearning-config-rabbitMQ-6001/src/main/java/com/misrobot/springcloud/Config_RabbitApplication.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@RefreshScope              ----------------->加载这里没有用 只能加在需要刷新的地方才可以 如${foo}
+//http://localhost:6001/bus/refresh
 public class Config_RabbitApplication {
 
     public static void main(String[] args) {
--- springcloudlearning-config-rabbitMQ-6001/src/main/java/com/misrobot/springcloud/controller/TestRabbit.java
+++ springcloudlearning-config-rabbitMQ-6001/src/main/java/com/misrobot/springcloud/controller/TestRabbit.java
@@ -3,18 +3,21 @@ package com.misrobot.springcloud.controller;
 import com.misrobot.springcloud.senders.Sender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
+//http://localhost:6001/bus/refresh
 public class TestRabbit {
 
     @Autowired
     Sender sender;
 
-    @Value("${foo:555}")
+    @Value("${foo:没有获取foo}")
     private String foo;
 
     @RequestMapping(value = "/getFoo")
